38431a284fe6fa0e647cbb6f79a929a4d9b5e1ed
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-08-16 17:58:47 -0400

    update flatpages and events

--- events/urls.py
+++ events/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('',
     url(r'^recent/(?P<year>\d{4})/$', 'act.events.views.event_recent_archive'),
     url(r'^recent/$', 'act.events.views.event_recent_archive', name='event_recent_archive'),
     
-    url(r'^upcoming/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'act.events.views.event_upcoming_archive'),
-    url(r'^upcoming/(?P<year>\d{4})/(?P<month>\d{2})/$', 'act.events.views.event_upcoming_archive'),
-    url(r'^upcoming/(?P<year>\d{4})/$', 'act.events.views.event_upcoming_archive'),
-    url(r'^upcoming/$', 'act.events.views.event_upcoming_archive', name='event_upcoming_archive'),
+    # url(r'^upcoming/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'act.events.views.event_upcoming_archive'),
+    # url(r'^upcoming/(?P<year>\d{4})/(?P<month>\d{2})/$', 'act.events.views.event_upcoming_archive'),
+    # url(r'^upcoming/(?P<year>\d{4})/$', 'act.events.views.event_upcoming_archive'),
+    # url(r'^upcoming/$', 'act.events.views.event_upcoming_archive', name='event_upcoming_archive'),
     
 )
