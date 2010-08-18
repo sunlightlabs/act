f7189713acb6807df9f895b03b54c38f4be080e5
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-08-18 15:20:30 -0400

    fix events

--- events/views.py
+++ events/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Min
 from django.shortcuts import render_to_response
 from django.views.generic import date_based
 from django.views.generic import list_detail
 from tagging.models import Tag
 from act.events.models import Event
+import datetime
 
 def event_index(request):
     data = {
@@ -12,59 +14,24 @@ def event_index(request):
     }
     return render_to_response('events/index.html', data)
 
-
-def event_recent_archive(request, year=None, month=None, day=None):
-    return event_archive(request, Event.objects.recent(), year, month, day)
-
-def event_upcoming_archive(request, year=None, month=None, day=None):
-    return event_archive(request, Event.objects.upcoming(), year, month, day)
-
-def event_archive(request, qs, year=None, month=None, day=None):
-    if day:
-        print "!!! day"
-        return date_based.archive_day(
-                    request,
-                    year=year,
-                    month=month,
-                    day=day,
-                    month_format='%m',
-                    date_field='start_date',
-                    allow_future=True,
-                    template_name='events/event_archive.html',
-                    template_object_name='event',
-                    queryset=qs)
-    elif month:
-        print "!!! month"
-        return date_based.archive_month(
-                    request,
-                    year=year,
-                    month=month,
-                    month_format='%m',
-                    date_field='start_date',
-                    allow_future=True,
-                    template_name='events/event_archive.html',
-                    template_object_name='event',
-                    queryset=qs)
-    elif year:
-        print "!!! year"
-        return date_based.archive_year(
-                    request,
-                    year=year,
-                    make_object_list=True,
-                    date_field='start_date',
-                    allow_future=True,
-                    template_name='events/event_archive.html',
-                    template_object_name='event',
-                    queryset=qs)
-    else:
-        print "!!! index"
-        return date_based.archive_index(
-                    request,
-                    date_field='start_date',
-                    allow_future=True,
-                    template_name='events/event_archive.html',
-                    template_object_name='event_list',
-                    queryset=qs)
+def event_archive(request, year=None):
+    this_year = datetime.datetime.today().year
+    min_year = Event.objects.aggregate(min_date=Min('start_date'))['min_date'].year
+    if not year:
+        year = this_year
+    qs = Event.objects.filter(start_date__year=year, is_public=True)
+    return date_based.archive_year(
+                request,
+                year=year,
+                make_object_list=True,
+                date_field='start_date',
+                allow_future=True,
+                template_name='events/event_archive.html',
+                template_object_name='event',
+                queryset=qs,
+                extra_context={
+                    'years': [y for y in range(min_year, this_year + 1)],
+                })
 
 def event_detail(request, year, month, day, slug):
     return date_based.object_detail(
